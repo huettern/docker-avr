f27d837215b191ea8573f5b5a3672ec809abbb2e
Noah Huetter <noahhuetter@gmail.com>
2018-02-20 22:00:01 +0100

    just playing

--- test/main.c
+++ test/main.c
@@ -2,13 +2,17 @@
 * @Author: noah
 * @Date:   2018-02-19 12:50:25
 * @Last Modified by:   Noah Huetter
-* @Last Modified time: 2018-02-19 14:14:37
+* @Last Modified time: 2018-02-20 19:14:22
 */
 
 #include <avr32/io.h>
 
+int main(void) {
+    AVR32_GPIO.port[3].gpers = 1 << 27;
+    AVR32_GPIO.port[3].oders = 1 << 27;
+    AVR32_GPIO.port[3].ovrs = 1 << 27;
 
-int main()
-{
+    for (;;) {}
+
     return 0;
 }
